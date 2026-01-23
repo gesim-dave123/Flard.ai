a9df54a725ea611631e69a7bd28d8988d059fccc
gesim-dave123 <christiandavegesim@gmail.com>
2026-01-23 16:17:25 +0800

    Initialized prisma

---
+++ server/src/lib/prisma.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from "../generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
+
+const connectionString = process.env.DATABASE_URL;
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+
+// Use a singleton pattern to prevent multiple active connections in dev mode
+const prismaClientSingleton = () => {
+  return new PrismaClient({ adapter });
+};
+
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
+}
+
+const prisma = globalThis.prisma ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
