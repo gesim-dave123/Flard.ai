cbbb5894159a4053868515ed97c1bf8c35447927
gesim-dave123 <christiandavegesim@gmail.com>
2026-02-06 00:33:13 +0800

    Feat: Added Frontend Routes for myClass,leaderboards and profile pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,9 @@ import Landing from "./pages/landing";
 import Login from "./pages/login";
 import Signup from "./pages/createAccount";
 import FlardDashboard from "./pages/dashboard";
+import MyClassesPage from "./pages/myClass";
+import LeaderboardPage from "./pages/leaderboards";
+import ProfilePage from "./pages/profile";
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/dashboard" element={<FlardDashboard />} />
+      <Route path="/myClasses" element={<MyClassesPage />} />
+      <Route path="/leaderboards" element={<LeaderboardPage />} />
+      <Route path="/profile" element={<ProfilePage />} />
     </Routes>
   );
 }
