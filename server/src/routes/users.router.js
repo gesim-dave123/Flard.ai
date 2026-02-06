6cc50b086a6a70c482b61e9548e2468ff7f8367f
gesim-dave123 <christiandavegesim@gmail.com>
2026-02-07 01:46:54 +0800

    Feat:Added the generate flashcard Route

--- server/src/routes/users.router.js
+++ server/src/routes/users.router.js
@@ -1,6 +1,8 @@
 import express from "express";
+import multer from "multer";
 import {loginUser} from "../controllers/auth.controller.js";
 import { registerUser, displayUserbyId } from "../controllers/users.controller.js";
+import { processPdfUpload } from "../controllers/flashcard.controller.js";
 
 const router = express.Router();
 
@@ -8,5 +10,11 @@ router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/:id", displayUserbyId);
 
+
+
+// routes/user.route.js
+const upload = multer({ dest: "uploads/" });
+// Frontend should call: /api/users/uploadFile
+router.post("/uploadFile", upload.single("file"), processPdfUpload);
 
 export default router;
