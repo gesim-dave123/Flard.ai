57850b90bf1a45713a3e009f82f2256c78a01560
gesim-dave123 <christiandavegesim@gmail.com>
2026-02-09 01:57:03 +0800

    Feat : Added pdf-parser and Integrated Gemini Flashcard generator function

--- server/src/controllers/flashcard.controller.js
+++ server/src/controllers/flashcard.controller.js
@@ -1,3 +1,5 @@
+import pdfParse from "@cedrugs/pdf-parse";
+import {runPrompt} from "../services/gemini.js";
 import fs from "fs";
 
 export const processPdfUpload = async (req, res) => {
@@ -5,21 +7,23 @@ export const processPdfUpload = async (req, res) => {
     if (!req.file) {
       return res.status(400).json({ error: "No PDF file uploaded." });
     }
-
-    const pdfParse = (await import("pdf-parse")).default; // dynamic import
     const dataBuffer = fs.readFileSync(req.file.path);
     const data = await pdfParse(dataBuffer);
-
     const extractedText = data.text;
+    // fs.unlinkSync(req.file.path);
+    console.log("PDF processed successfully. Extracted text length:", extractedText.length);
+    console.log("Now forging flashcards with Gemini...");
+    const flashcards = await runPrompt(extractedText);
 
-    // Cleanup uploaded file
-    fs.unlinkSync(req.file.path);
+    console.log("Flashcards generated successfully:", flashcards);
+
 
     res.status(200).json({
       success: true,
       message: "Intel extracted! Ready to forge flashcards.",
-      text: extractedText,
+      
     });
+  
   } catch (error) {
     console.error("Extraction Error:", error);
     res.status(500).json({ error: "The system failed to read the scroll." });
